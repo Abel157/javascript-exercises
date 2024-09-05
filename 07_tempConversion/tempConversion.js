471230f7e647585be03e9cda1ba805eb2655bc2b
Abel157 <abelsewinet1213@gmail.com>
2024-09-05 18:41:15 +0300

    do 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tf) {
+  return (Math.round((5/9*(tf - 32))*10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tc) {
+  return (Math.round((9/5 *tc + 32)*10)/10);
 };
-
+console.log(convertToFahrenheit(23));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
