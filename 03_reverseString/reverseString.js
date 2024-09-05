97461eb1fab0b46f6185aa3f6401ed110461b70a
Abel157 <abelsewinet1213@gmail.com>
2024-09-05 16:46:50 +0300

    do 05

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strarr=str.split("");
+    console.log(strarr);
+    strarr.reverse();
+    const result=strarr.join("");
+    console.log(result);
+    return result;
 };
+reverseString("hjy lmn");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function() {// if the rest paramater was used it would be function( arr, ...args);
+    const args=Array.from(arguments);
+    const arr=args[0];
+    args.splice(0,1);
+    for (let items of args){         // instead of splicing the old array i could make a new array by pushing values                                 
+        let i=0;                    //    in the old array that are not in the args " also easier to remove duplications since "
+        while (i < arr.length){      //       if a value occurs atleast once it will not be pushed. 
+            if (items===arr[i]){
+                arr.splice(i,1);
+                i-=1
+            }
+            else{
+                i++;
+            }
+        }
+        
+    }
+   return(arr);
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
