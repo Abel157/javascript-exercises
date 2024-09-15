351c446d03994ffe381b3efe6183e4e1a656d650
Abel157 <abelsewinet1213@gmail.com>
2024-09-15 09:22:44 +0300

    Do excersise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return(a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum , current) => sum+=current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, curr) => mul*=curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let arr=[];
+  while (num>0){
+    arr.push(num);
+    num--;
+  }
+  return arr.reduce((fact,curr)=>fact*=curr,1);
 };
 
 // Do not edit below this line
