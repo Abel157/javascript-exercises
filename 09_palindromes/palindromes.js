34719829f062de72dc35afb0dcb1cf230b63f96d
Abel157 <abelsewinet1213@gmail.com>
2024-09-15 10:15:43 +0300

    Do exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let arr=[];
+    let wordarr=[];
+    let lettnums="abcdefghijklmnopqrstuvwxyz0123456789";
+    for (char of word){
+        if (!lettnums.includes(char.toLowerCase())) continue;
+        else{
+            wordarr.push(char);
+            arr.unshift(char);
+        }
+    }
+    const original=wordarr.toString().toLowerCase();
+    const reverse=arr.toString().toLowerCase();
+    return (original===reverse);
 };
-
+console.log(palindromes("racecar"));
 // Do not edit below this line
 module.exports = palindromes;
