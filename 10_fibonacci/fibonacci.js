7bc76e385c38b3fbc162e9f64769ba33da6c33b2
Abel157 <abelsewinet1213@gmail.com>
2024-09-15 13:36:32 +0300

    Do excersise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arr=[1,1]
+    let prev = 0;
+    let curr=1;
+    let next=0;
+    if (+num==0) return 0;
+    if (+num<0) return "OOPS";
 
-};
+    if (+num>2){
+        while (num-2){
+            next=arr[prev] + arr[curr];
+            arr.push(next);
+            prev+=1;
+            curr+=1
+            num--;
+        }
+        return next;
+    }
+    else {
+        return arr[+num-1];
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
