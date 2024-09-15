cb638129a929e089a1af9e7a19f30410411d80e7
Abel157 <abelsewinet1213@gmail.com>
2024-09-15 16:07:19 +0300

    Do exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let input=arr.slice();
+    let currentyear=new Date();
+    for (persons of input){
+        persons.age=((persons.yearOfDeath)? persons.yearOfDeath: currentyear.getFullYear())-persons.yearOfBirth;
+    };
+    return input.sort((a,b)=> a.age - b.age)[input.length -1];
+    
+        
+        
+    
 };
-
+console .table(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]));
 // Do not edit below this line
 module.exports = findTheOldest;
